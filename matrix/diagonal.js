4df747703a6ae4a15c0ad62fdbee67878037dcea
mvikast7 <mvikastiwari7@gmail.com>
2022-08-04 17:45:17 +0530

    Completed diagonal difference program

--- matrix/diagonal.js
+++ matrix/diagonal.js
@@ -1,8 +1,8 @@
-let arr = []
+let arr = [ [ 11, 2, 4 ], [ 4, 5, 6 ], [ 10, 8, -12 ] ]
 let difference = 0
 for(let ind in arr){
     difference += arr[ind][ind]
     difference -= arr[arr.length - ind - 1][ind]
 
 }
-return Math.abs(difference) 
+console.log(Math.abs(difference)) 
